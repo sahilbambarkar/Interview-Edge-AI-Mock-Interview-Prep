afa10e217fb0679719194789b3d8c97fa6aaba98
sahilbambarkar <162725936+sahilbambarkar@users.noreply.github.com>
2025-02-13 19:53:16 +0530

    mobile logo changed

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -41,10 +41,11 @@ export default function Page() {
               >
                 <span className="sr-only">Home</span>
                 <Image
-                  src="/Logo4.png"
+                   src="/logo1.png"
                   alt="my responsive logo"
-                  width={50}
-                  height={50}
+                  width={85}
+                  height={85}
+
                 />
               </a>
 
